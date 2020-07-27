1df3a2d40217979be04b3c53cc980d33c2bed3f5
ulempicka <urszula.lempicka@gmail.com>
2020-07-27 14:42:26 +0200

    Commit 7 - updated competition

--- src/sportCompetition/TestAthletes.java
+++ src/sportCompetition/TestAthletes.java
@@ -11,20 +11,20 @@ public class TestAthletes {
         List<Athlete> athletes = AthleteGenerator.createAthlete();
         System.out.println(athletes);
 
-//        String fileName = "src\\sportCompetition\\stats.csv";
-//        File newFile = new File(fileName);
-//
-//        try (
-//                var fileWriter = new FileWriter(newFile);
-//                var bufferedWriter = new BufferedWriter(fileWriter)
-//        ) {
-//            for (Athlete athlete : athletes) {
-//                bufferedWriter.write(athlete.getFirstName() + ";" + athlete.getLastName() + ";" + athlete.getScore());
-//                bufferedWriter.newLine();
-//            }
-//        } catch (IOException e) {
-//            System.err.println("nie mozna odczytac pliku");
-//        }
+        String fileName = "src\\sportCompetition\\stats.csv";
+        File newFile = new File(fileName);
+
+        try (
+                var fileWriter = new FileWriter(newFile);
+                var bufferedWriter = new BufferedWriter(fileWriter)
+        ) {
+            for (Athlete athlete : athletes) {
+                bufferedWriter.write(athlete.getFirstName() + ";" + athlete.getLastName() + ";" + athlete.getScore());
+                bufferedWriter.newLine();
+            }
+        } catch (IOException e) {
+            System.err.println("nie mozna odczytac pliku");
+        }
     }
 }
 
